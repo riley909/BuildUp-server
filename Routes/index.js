5042d0b761b483687e231456ebc0ff2712ade757
riley909 <increaseblue666@gmail.com>
2021-03-20 22:03:58 +0900

    Fix #13-edit:Routes/index.js

--- Routes/index.js
+++ Routes/index.js
@@ -1,8 +1,4 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
-});
-
 module.exports = router;
