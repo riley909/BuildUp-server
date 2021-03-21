bcbbe28c16605eb079de2636aa56718e71fb19c9
riley909 <increaseblue666@gmail.com>
2021-03-21 18:18:01 +0900

    Fix #46-edit:models/achievment.js

--- models/achievment.js
+++ models/achievment.js
@@ -1,30 +1,29 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class achievment extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
+  class achievment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
 
-        static associate(models) {
-            // define association here
-            achievment.belongsTo(models.user, { foreignKey: 'user_id' });
-            achievment.belongsTo(models.date, { foreignKey: 'date_id' });
-        }
-    };
-    achievment.init(
-        {
-            user_id: DataTypes.INTEGER,
-            date_id: DataTypes.INTEGER,
-            rate: DateTypes.INTEGER
-        },
-        {
-            sequelize,
-            modelName: 'achievment',
-        }
-    );
-    return achievment;
+    static associate(models) {
+      // define association here
+      achievment.belongsTo(models.user, { foreignKey: 'user_id' });
+      achievment.belongsTo(models.date, { foreignKey: 'date_id' });
+    }
+  }
+  achievment.init(
+    {
+      user_id: DataTypes.INTEGER,
+      date_id: DataTypes.INTEGER,
+      rate: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'achievment',
+    }
+  );
+  return achievment;
 };
-
